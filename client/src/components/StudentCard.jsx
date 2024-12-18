fdd39aed3e041341b4670262533504ad04a07541
דניאל המנשין <danham531@gmail.com>
2024-12-18 22:17:14 +0200

    logout button working and student card component and css init

---
+++ client/src/components/StudentCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useUserContext } from '../contexts/UserContext'
+import { Person } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import style from '../styles/studentCard.module.css'
+//TODO: only init need css and fixing the bg img
+const StudentCard = () => {
+    const { user } = useUserContext();
+
+    return (
+        <>
+            <div className={style.card}>
+                {user.img ? <img src={user.img} alt="" /> : <IconButton><Person /></IconButton>}
+                <h2>{user.name}</h2>
+                {/* <h3>Student id{user.StudentNumber}</h3> */}
+                <h2>Major: {user.major}</h2>
+                <h3>Registeration date: {user.enrolledDate.split("T")[0]}</h3>
+            </div>
+        </>
+    )
+}
+
+export default StudentCard
--- client/src/pages/PersonalArea.jsx
+++ client/src/pages/PersonalArea.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
-
+import StudentCard from '../components/StudentCard'
+// TODO: only added studens card
 const PersonalArea = () => {
     return (
         <>
             <Header />
+            <StudentCard />
         </>
     )
 }
