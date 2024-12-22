58048e6b102bf470d508dde384c1d8857610cecf
דניאל המנשין <danham531@gmail.com>
2024-12-22 21:33:45 +0200

    optional courses choosing almost done

--- client/src/pages/PersonalArea.jsx
+++ client/src/pages/PersonalArea.jsx
@@ -4,21 +4,36 @@ import StudentCard from '../components/StudentCard'
 import { useUserContext } from '../contexts/UserContext'
 import style from '../styles/personal.module.css'
 import axios from 'axios'
-import { OPTIONAL_COURSES_URL } from '../constants/endPoint'
+import { OPTIONAL_COURSES_CHOOSE_URL, OPTIONAL_COURSES_URL } from '../constants/endPoint'
 import Feedback from '../components/Feedback'
 // TODO: only added studens card
 const PersonalArea = () => {
     const { user } = useUserContext();
     const [optionalCourses, setOptionalCourses] = useState([]);
+    const [maxChoises, setMaxChoises] = useState(0);
     const [chosenCourses, setChosenCourses] = useState([]);
     const [currentCourse, setCurrentCourse] = useState("");
     const [isCoursesFull, setIsCoursesFull] = useState(false);
+    const [fullCourses, setFullCourses] = useState([]);
+    const [error, setError] = useState("");
 
 
     const getOptionalCourses = async () => {
         try {
             const { data } = await axios.get(OPTIONAL_COURSES_URL + user._id);
-            setOptionalCourses(data);
+            setOptionalCourses(data.courses);
+            setMaxChoises(data.max);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const updateCourses = async () => {
+        try {
+            const { data } = await axios.put(OPTIONAL_COURSES_CHOOSE_URL + user._id, { courses: chosenCourses });
+            setFullCourses(data);
+            console.log("updated");
+
         } catch (error) {
             console.error(error);
         }
@@ -28,9 +43,11 @@ const PersonalArea = () => {
         getOptionalCourses();
     }, []);
 
+
+
     const chooseCourses = (e) => {
         const { value, checked } = e.target; // Get the value and checked state of the checkbox
-
+        setError("");
         setChosenCourses((prev) => {
             if (checked) {
                 // Add the course if it's checked
@@ -55,7 +72,7 @@ const PersonalArea = () => {
 
 
                 {!isCoursesFull && <div className={style.optionalCourses}>
-                    <h4>select optional courses</h4>
+                    <h4>select optional courses max: {maxChoises}</h4>
                     {optionalCourses.map((item, index) => {
                         return (
                             <div className={style.option} key={index}>
@@ -69,7 +86,15 @@ const PersonalArea = () => {
                             </div>
                         );
                     })}
-
+                    <button onClick={() => {
+                        if (chosenCourses.length == maxChoises) {
+                            updateCourses();
+                        }
+                        else {
+                            setError(`please choose ${maxChoises} courses`);
+                        }
+                    }}>aplly</button>
+                    {error && <p>{error}</p>}
                 </div>}
 
                 <Feedback />
