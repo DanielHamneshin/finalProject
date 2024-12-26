5b489a3fb98ae8e0f25744287f05bf9a3677c78c
eliyaYakkov <eliya.yacov@gmail.com>
2024-12-26 09:32:10 +0200

    add createLesson student:add presence and change image to Buffer

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -2,7 +2,7 @@ const Student = require("../models/studentModel")
 const Lesson  = require("../models/lessonModel")
 const Major = require("../models/majorModel")
 const Course = require("../models/courseModel")
-
+const Teacher = require("../models/teacherModel")
 
 
 // courses when not selected
@@ -44,14 +44,21 @@ exports.getAllCourses = async (req, res) => {
         let courses=[]
         if(role === "student"){
             const student = await Student.findById(req.params.userId);
-            courses = student.courses
+            courses = [...student.courses]
         }
         else if(role === "teacher"){
             const teacher = await Teacher.findById(req.params.userId);
-            courses = teacher.courses
+            courses = [...teacher.courses]
         }
+        console.log(courses);
 
-        res.status(200).json(courses.map(course => course.name))    
+        const courseNames = await Promise.all(courses.map(async(courseId)=>{
+            const course = await Course.findById(courseId)
+            return course.name
+        }))
+        console.log(courseNames);
+        
+        res.status(200).json(courseNames)    
     }catch(error){
         console.log(error);
         res.status(500).json({ msg: error })
