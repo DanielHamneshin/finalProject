570d5395fc79c0dd1e73fd717e4dff0a8851acf2
eliyaYakkov <eliya.yacov@gmail.com>
2024-12-29 09:23:32 +0200

    chnaged getStudentAttendance and adding getAllStudentByCourse adding unique to course name and adding all the routes for mashov

--- server/models/courseModel.js
+++ server/models/courseModel.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 const courseSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique:true
     },
     teacherName: {
         type: String,
